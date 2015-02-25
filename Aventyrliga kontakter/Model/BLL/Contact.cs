c6c029e670511142405b871c782809e5af2487f1
MartinArvidsson <ma223ku@lnu.student.se>
2015-02-25 17:04:06 +0100

    Working

---
+++ Aventyrliga kontakter/Model/BLL/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aventyrliga_kontakter.BLL
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+
+        [Required(ErrorMessage = "En Epost måste anges.")]
+        [StringLength(30, ErrorMessage = "Max 30 tecken.")]
+        public string EmailAdress { get; set; }
+
+        [Required(ErrorMessage = "Ett förnamn måste anges.")]
+        [StringLength(30, ErrorMessage = "Max 30 tecken.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Ett efternamn måste anges.")]
+        [StringLength(30, ErrorMessage = "Max 30 tecken.")]
+        public string LastName { get; set; }
+    }
+}
---
+++ Aventyrliga kontakter/Model/DAL/ContactDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Aventyrliga_kontakter.DAL
+{
+    public class ContactDAL
+    {
+        
+        private static string _connectionString;
+
+        static ContactDAL()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings["Sträng här"].ConnectionString;
+        }
+
+        private static SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+
+        }
+    }
+}
