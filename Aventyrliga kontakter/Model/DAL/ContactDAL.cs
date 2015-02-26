67cdafe6913c6f55a76bc9f7c01557a3b002ab19
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-02-26 11:45:59 +0100

    Working

--- Aventyrliga kontakter/Model/DAL/ContactDAL.cs
+++ Aventyrliga kontakter/Model/DAL/ContactDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Aventyrliga_kontakter.BLL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -9,7 +11,7 @@ namespace Aventyrliga_kontakter.DAL
 {
     public class ContactDAL
     {
-        
+        // https://github.com/1dv406/kursmaterial/blob/master/Exempel/Kundkontakter%20(version%205%2C%20Flera%20sidor)/GeekCustomer/Model/DAL/CustomerDAL.cs
         private static string _connectionString;
 
         static ContactDAL()
@@ -24,7 +26,159 @@ namespace Aventyrliga_kontakter.DAL
 
         public IEnumerable<Contact> GetContacts()
         {
+            using(var conn= CreateConnection())
+            {
+                try
+                {
+                    var contacts = new List<Contact>(1000);
 
+                    var cmd = new SqlCommand("app.uspGetContacts", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    using(var reader = cmd.ExecuteReader())
+                    {
+                        var contactIdIndex = reader.GetOrdinal("contactID");
+                        var EmailAdressIndex = reader.GetOrdinal("EmailAdress");
+                        var FirstNameIndex = reader.GetOrdinal("FirstName");
+                        var LastNameIndex = reader.GetOrdinal("LastName");
+
+                        while(reader.Read())
+                        {
+                            contacts.Add(new Contact
+                                {
+                                    ContactID = reader.GetInt32(contactIdIndex),
+                                    EmailAdress = reader.GetString(EmailAdressIndex),
+                                    FirstName = reader.GetString(FirstNameIndex),
+                                    LastName = reader.GetString(LastNameIndex)
+                                });
+                        }
+                    }
+
+                    contacts.TrimExcess();
+
+                    return contacts;
+                }
+                catch
+                {
+                    throw new ApplicationException("Fan, något gick fel när data hämtades från databasen");
+                }
+            }
+        }
+
+        public Contact GetContactById(int contactId)
+        {
+            using(SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app.uspGetContacts", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@contactId", contactId);
+
+                    conn.Open();
+
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+                           int contactIdIndex = reader.GetOrdinal("contactID");
+                           int EmailAdressIndex = reader.GetOrdinal("EmailAdress");
+                           int FirstNameIndex = reader.GetOrdinal("FirstName");
+                           int LastNameIndex = reader.GetOrdinal("LastName");
+
+                           return new Contact
+                           {
+                               ContactID = reader.GetInt32(contactIdIndex),
+                               EmailAdress = reader.GetString(EmailAdressIndex),
+                               FirstName = reader.GetString(FirstNameIndex),
+                               LastName = reader.GetString(LastNameIndex)
+                           };
+                        }
+                    }
+                    return null;
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel i dataåtkomstlagret ;_;");
+                }
+            }
+        }
+        public void InsertContact(Contact contact)
+        {
+            using(SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app.uspInsertContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@EmailAdress", sqlDbType.VarChar, 50).Value = contact.EmailAdress;
+                    cmd.Parameters.Add("@Förnamn", sqlDbType.VarChar, 50).Value = contact.FirstName;
+                    cmd.Parameters.Add("@Efternamn", sqlDbType.VarChar, 50).Value = contact.LastName;
+
+                    cmd.Parameters.Add("@ContactId", sqlDbType.Int, 5).Direction = ParameterDirection.Output;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    contact.ContactID = (int)cmd.Parameters["@ContactId"].Value;
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel i dataåtkomstlagret ;-;");
+                }
+            }
+        }
+
+        public void UpdateContact(Contact contact)
+        {
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app.uspUpdateContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@ContactId", sqlDbType.Int, 4).Value = contact.ContactID;
+                    cmd.Parameters.Add("@EmailAdress", sqlDbType.VarChar, 50).Value = contact.EmailAdress;
+                    cmd.Parameters.Add("@Förnamn", sqlDbType.VarChar, 50).Value = contact.FirstName;
+                    cmd.Parameters.Add("@Efternamn", sqlDbType.VarChar, 50).Value = contact.LastName;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+                    // Kastar ett eget undantag om ett undantag kastas.
+                    throw new ApplicationException("Fel i data åtkomstlagret .. ._.");
+                }
+            }
+        }
+        public void DeleteContact(int contactId)
+        {
+            using(SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app.uspDeleteContact", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@contactId", sqlDbType.Int, 4).Value = contactId;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel i dataåtkomstlagret ,___,");
+                }
+            }
         }
     }
 }
